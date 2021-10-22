6a96eb0f3316bf33f82bd5e097cab4be264f1415
ines khelifi <52375090+ineskh12@users.noreply.github.com>
2021-10-22 16:18:59 +0100

    mail test

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
-    $to      = 'nobody@example.com';
+    $to      = 'ali.obba.chantier@gmail.com';
     $subject = 'the subject';
     $message = 'hello';
-    $headers = 'From: ali.obba.chantier@gmail.comm'       . "\r\n" .
+    $headers = 'From: ali.obba.chantier@gmail.com'       . "\r\n" .
                  'Reply-To: ali.obba.chantier@gmail.com' . "\r\n" .
                  'X-Mailer: PHP/' . phpversion();
 
